8151e5186f54b908b5ce719059e6189a724e9e75
Robert Forkel <xrotwang@googlemail.com>
2022-03-10 08:10:02 +0100

    release 1.2.0

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ import codecs
 setup(
     name='pyigt',
     description="A Python library for handling inter-linear-glossed text.",
-    version='1.1.1.dev0',
+    version='1.2.0',
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -32,8 +32,8 @@ setup(
     url='https://github.com/cldf/pyigt',
     long_description=codecs.open('README.md', 'r', 'utf-8').read(),
     long_description_content_type='text/markdown',
-    author='Johann-Mattis List',
-    author_email='list@shh.mpg.de',
+    author='Johann-Mattis List and Robert Forkel',
+    author_email='mattis_list@eva.mpg.de',
     keywords='Chinese linguistics, historical linguistics, computer-assisted language comparison',
     extras_require={
         'dev': ['flake8', 'wheel', 'twine'],
--- src/pyigt/__init__.py
+++ src/pyigt/__init__.py
@@ -1,4 +1,4 @@
 from .igt import Corpus, IGT  # noqa: F401
 from .lgrmorphemes import GlossedWord  # noqa: F401
 
-__version__ = '1.1.1.dev0'
+__version__ = '1.2.0'
